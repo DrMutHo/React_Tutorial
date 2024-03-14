b35504cad0717f9390983ffdde7e0a863a9b8149
Bach Nguyen <bachbet34@gmail.com>
2024-03-14 21:05:36 +0700

    17

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 class MyComponent extends React.Component {
+
+    state = {
+        name: 'ssSeul',
+        address: 'UET',
+        age: 19
+    }
+
     render() {
         return (
-            <div>my first component
-                {Math.random()}
+            <div>
+                My name is {this.state.name}
+
             </div>
         )
     }
