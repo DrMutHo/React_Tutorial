3431edaa5a1bdadceb6e32ce881af2e1f189d539
Bach Nguyen <bachbet34@gmail.com>
2024-03-15 18:11:47 +0700

    25

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,21 +1,44 @@
 import React from "react";
 
 class DisplayInfo extends React.Component {
-    render() {
 
+    state = {
+        isShowListUsers: true
+    }
+
+
+    handleShowHide = () => {
+        this.setState({
+            isShowListUsers: !this.state.isShowListUsers
+        })
+    }
+
+
+
+    render() {
         const { listUsers } = this.props;
         return (
             <div>
-                {listUsers.map((user) => {
-                    return (
-                        <div key={user.id}>
-                            <div>My name's {user.name}</div>
-                            <div>My age's {user.age}</div>
-                            <hr />
-                        </div>
-                    )
-                })}
-            </div >
+                <div>
+                    <span onClick={(event) => { this.handleShowHide(event) }}>
+                        {this.state.isShowListUsers === true ? "Hide list users" : "Show list users"}
+                    </span>
+                </div>
+                {this.state.isShowListUsers &&
+                    <div>
+                        {listUsers.map((user) => {
+
+                            return (
+                                <div key={user.id} className={+user.age > 25 ? "green" : "red"}>
+                                    <div>My name's {user.name}</div>
+                                    <div>My age's {user.age}</div>
+                                    <hr />
+                                </div>
+                            )
+                        })}
+                    </div>
+                }
+            </div>
         )
     }
 }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,8 +7,8 @@ class MyComponent extends React.Component {
     state = {
         listUsers: [
             { id: 1, name: "ssSeul", age: "19" },
-            { id: 2, name: "Mut Ho", age: "19" },
-            { id: 3, name: "Bach", age: "19" },
+            { id: 2, name: "Mut Ho", age: "68" },
+            { id: 3, name: "Bach", age: "86" },
         ]
     }
 
