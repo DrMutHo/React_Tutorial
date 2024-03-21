99be632b681878bcb1c1aab4dc7ec080517991f8
Bach Nguyen <bachbet34@gmail.com>
2024-03-21 21:54:18 +0700

    49

--- src/index.js
+++ src/index.js
@@ -5,14 +5,30 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import User from './components/User/User';
+import Admin from './components/Admin/Admin';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
+    {/* <React.StrictMode>
       <App />
-    </React.StrictMode>
-  </Provider>
+    </React.StrictMode> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} >
+          <Route path="users" element={<User />} />
+          <Route path="admins" element={<Admin />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
+  </Provider >
 );
 
 // If you want to start measuring performance in your app, pass a function
