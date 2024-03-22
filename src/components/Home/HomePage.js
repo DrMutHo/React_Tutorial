3c6568ed08e3df684f3c03d89b05efcee74afa08
Bach Nguyen <bachbet34@gmail.com>
2024-03-22 17:38:15 +0700

    53

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -9,6 +9,24 @@ const HomePage = (props) => {
                     type="video/mp4"
                 />
             </video>
+
+            <div className="homepage-content">
+                <div className="title-1">
+                    Make forms
+                    <br />
+                    worth filling out
+                </div>
+                <div className="title-2">
+                    <span>
+                        "Get more data—like signups, feedback, and anything else—with forms designed to be "
+                        <strong>refreshingly different.</strong>
+                    </span>
+
+                </div>
+                <div className="title-3">
+                    <button className="btn-getstart">Get started-it's free</button>
+                </div>
+            </div>
         </div>
     );
 }
