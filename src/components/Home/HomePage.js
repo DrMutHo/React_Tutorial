5e62857edc8350d69c2989835ed71cc7c68917c1
Bach Nguyen <bachbet34@gmail.com>
2024-03-22 15:16:56 +0700

    52

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,6 +1,15 @@
+import vidHomePage from "../../assets/hero.mp4";
+
 const HomePage = (props) => {
     return (
-        <div className="homepage-container">Home page components</div>
+        <div className="homepage-container">
+            <video loop autoPlay muted >
+                <source
+                    src={vidHomePage}
+                    type="video/mp4"
+                />
+            </video>
+        </div>
     );
 }
 
